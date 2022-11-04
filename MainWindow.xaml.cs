fd29bf8410e0569873b306f7023e293889bc45a6
andriesk <andries@cadsharp.com>
2022-11-04 07:47:31 -0300

    Added progress reporting

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,42 @@ namespace Camman_Drawing_Manager
     /// </summary>
     public partial class MainWindow : Window
     {
+        public class ProgramProgres
+        {
+            public int ProgressValue { get; set; }
+            public string ProgressDescription { get; set; } = "";
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            var progress = new Progress<ProgramProgres>(pp =>
+            {
+                pbrProgress.Minimum = 1;
+                pbrProgress.Maximum = 10;
+                pbrProgress.Value = pp.ProgressValue;
+                pbrDescription.Content = pp.ProgressDescription;
+            });
+            ProcessFiles(progress);
+
+        }
+
+        public async void ProcessFiles(IProgress<ProgramProgres> progress)
+        {
+            // loop 10 times
+            for (int i = 1; i <= 10; i++)
+            {
+                // simulate a long running task
+                await Task.Delay(1000);
+
+                // report progress
+                progress.Report(new ProgramProgres { ProgressValue = i, ProgressDescription = "Processing file " + i.ToString() });
+            }
         }
     }
 }
